ad0bdd1ca8fd1b07782a037d0013c2c21233002d
WardAarcden <ivanov33.aa@edu.spbstu.ru>
2024-04-07 16:46:20 +0300

    update2

--- Assets/Scripts/UnitAttributes.cs
+++ Assets/Scripts/UnitAttributes.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 [System.Serializable]
 public class EntityData
 {
-    public string id;
+    public string id { get; }
     public string Name;
     public string type;
     public float MoveSpeed;
@@ -16,6 +16,18 @@ public class EntityData
     public int Health;
     public int Durability;
     public int Vulnerability;
+
+
+    EntityData(string Id)
+    {
+        id = Id;
+
+        getConfig(id);
+    }
+    private void getConfig(string id)
+    {
+
+    }
 }
 
 [System.Serializable]
@@ -24,23 +36,30 @@ public class EntityList
     public Dictionary<string, EntityData> Entity;
     public Dictionary<string, Dictionary<string, string>> TownHolders;
 }
-public class UnitAttributes : MonoBehaviour
+public class ConfigApi : MonoBehaviour
 {
-        public void Start()
-        {
-            string fileName = "config.json";
-            string path = Path.Combine(Application.dataPath, "Scripts", fileName);
+    private EntityList entityList = null;
+    public void Start()
+    {
+        string fileName = "config.json";
+        string path = Path.Combine(Application.dataPath, "Scripts", fileName);
 
-            if (File.Exists(path))
-            {
-                string json = File.ReadAllText(path).Trim();
-            EntityList entityList = JsonConvert.DeserializeObject<EntityList>(json) ??
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path).Trim();
+            entityList = JsonConvert.DeserializeObject<EntityList>(json) ??
                                     throw new InvalidOperationException(
                                         "Failed to deserialize json to EntityList object.");
-            }
-            else
-            {
-                Debug.Log("File " + fileName + " not found.");
-            }
         }
+        else
+        {
+            Debug.Log("File " + fileName + " not found.");
+        }
+    }
+
+    public EntityData GetConfig(string id)
+    {
+        ;
+    }
+
 }
--- Assets/Scripts/UnitType.cs
+++ Assets/Scripts/UnitType.cs
@@ -10,32 +10,21 @@ public class UnitType : MonoBehaviour
 {
     [SerializeField]
     private string id;
+    private EntityList entityList;
+    [SerializeField]
     private EntityData entityData;
+
     public void Start()
     {
-        string fileName = "config.json";
-        string path = Path.Combine(Application.dataPath, "Scripts", fileName);
-
-        if (File.Exists(path))
+        foreach (var entity in entityList.Entity)
         {
-            string json = File.ReadAllText(path).Trim();
-            EntityList entityList = JsonConvert.DeserializeObject<EntityList>(json) ??
-                                    throw new InvalidOperationException(
-                                        "Failed to deserialize json to EntityList object.");
-            foreach (var entity in entityList.Entity)
+            EntityData data = entity.Value;
+            data.id = entity.Key;
+            if (data.id == id)
             {
-                EntityData data = entity.Value;
-                data.id = entity.Key;
-                if (data.id == id)
-                {
-                    entityData = data;
-                }
-                Debug.Log("Entity ID: " + data.id);
+                entityData = data;
             }
-        }
-        else
-        {
-            Debug.Log("File " + fileName + " not found.");
+            Debug.Log("Entity ID: " + data.id);
         }
     }
 }
